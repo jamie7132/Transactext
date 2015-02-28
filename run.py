f5804f74f318f5aa436e8e6bf53c6d4a3ce6d0eb
jamie7132 <jfsmills@umich.edu>
2015-02-28 17:44:01 -0600

    basic response done

--- run.py
+++ run.py
@@ -6,9 +6,23 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
- 
+
+    #print request.values
+    body = request.values['Body']
+    tokens = body.split()
+
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    
+    if len(tokens) == 2:
+        if body.lower().startswith('pay'):
+            resp.message("Paying $" + str(tokens[1]))
+        elif body.lower().startswith('request'):
+            resp.message("Request $" + str(tokens[1]))
+        else:
+            resp.message("Invalid demand - Message did not start with 'PAY' or 'REQUEST'")
+    else:
+        resp.message("Invalid demand - Message must be of format: 'PAY'/'REQUEST' AMOUNT")
+    
     return str(resp)
  
 if __name__ == "__main__":
